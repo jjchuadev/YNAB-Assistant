d3cde75650af4e43b66400820ecb1fb344783e63
John Chua <jjchuadev@gmail.com>
2019-04-29 21:43:00 -0700

    Initializing project

---
+++ YNAB-Assistant/src/app/shared/library.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+@NgModule({
+	imports: [
+		FlexLayoutModule
+	]
+})
+/*
+Pass your application module to the bootstrapModule function located in main.ts to start your app
+*/
+export class LibraryModule { }
